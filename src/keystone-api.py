d1aafebf344c8d887f053f3ebaccd9fead5294e1
Piyush Harsh <piyush.harsh@zhaw.ch>
2013-11-14 16:29:20 +0100

    refined to extract the endpoints

--- src/keystone-api.py
+++ src/keystone-api.py
@@ -1,3 +1,4 @@
+# -*- coding: ascii -*-
 #--------------------------------------------------------------
 #Created on Nov 11, 2013
 #
@@ -9,8 +10,9 @@
 #@requires: python 2.7
 #--------------------------------------------------------------
 
+
 import httplib2 as http
-import sys
+import sys, re
 import json
 import getpass
 
@@ -19,6 +21,11 @@ try:
 except ImportError:
     from urllib.parse import urlparse
     
+def strip_non_ascii(string):
+    ''' Returns the string without non ASCII characters'''
+    stripped = (c for c in string if 0 < ord(c) < 127)
+    return ''.join(stripped)
+
 def login():
     user = raw_input("Username [%s]: " % getpass.getuser())
     tenant = raw_input("Tenant Id: ")
@@ -28,11 +35,25 @@ def login():
     p1 = pprompt()
     return user, p1, tenant
 
+def get_endpoints(tokenId):
+    headers = {
+               'Accept': 'application/json',
+               'Content-Type': 'application/json;'
+    }
+    uri = 'http://160.85.4.11:5000' #replace this with the API end-point of your setup
+    path = '/v2.0/tokens/' + tokenId + '/endpoints'
+    target = urlparse(uri+path)
+    method = 'GET'
+    body = ''
+    h = http.Http()
+    response, content = h.request(target.geturl(),method,body,headers)
+    print "Endpoints:\n" + content
+
 def main(argv):
     print "Hello There. This is a simple test application making a test API call to OS"
     headers = {
                'Accept': 'application/json',
-               'Content-Type': 'application/json; charset=UTF-8'
+               'Content-Type': 'application/json;'
     }
     uri = 'http://160.85.4.11:5000' #replace this with the API end-point of your setup
     path = '/v2.0/tokens'
@@ -49,8 +70,9 @@ def main(argv):
     print "The data received is:\n" + content
     header = json.dumps(response)
     json_header = json.loads(header)
-    server_response = json_header["status"].encode('ascii','ignore')
+    server_response = json_header["status"]
     print "Server response: " + server_response
+    
     if server_response not in {'200'}: 
         print "Something went wrong!"
     else:
@@ -60,6 +82,11 @@ def main(argv):
         print "Token-id: " + data["access"]["token"]["id"]
         print "Username: " + data["access"]["user"]["username"]
         print "User-id: " + data["access"]["user"]["id"]
+        print "Catalog List:"
+        for i in range(len(data["access"]["serviceCatalog"])):
+            catalog_element =  data["access"]["serviceCatalog"][i]
+            print catalog_element["name"] + ", " + catalog_element["type"] + ", endpoint: " + catalog_element["endpoints"][0]["publicURL"]
+            
     return True
     
 if __name__ == '__main__':
