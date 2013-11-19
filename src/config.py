a55880ceaefd2ea4336e9cfd08aef2f152b133fc
Piyush Harsh <piyush.harsh@zhaw.ch>
2013-11-19 11:48:39 +0100

    removed old code

--- src/config.py
+++
@@ -1,9 +0,0 @@
-############################################################
-#@author: Piyush Harsh (piyush.harsh@zhaw.ch)
-#@version: 0.1
-#@summary: Module implementing RCB Rest Interface
-#
-#@requires: bottle
-############################################################
-
-dbPath = None
