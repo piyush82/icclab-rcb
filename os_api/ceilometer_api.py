b1d85d4f9b7c1d1b342e54553633b2ddcd8e231e
teaklvs <tea.kolevska@gmail.com>
2014-01-08 14:05:31 +0100

    addes meter statistics

--- os_api/ceilometer_api.py
+++ os_api/ceilometer_api.py
@@ -56,3 +56,54 @@ def get_meter_list(token, api_endpoint):
             meter_list[i]["meter-unit"] = data[i]["unit"]
         #print data
     return True, meter_list
+
+def period():
+    from_date = raw_input("Month from yyyy-mm-dd: ")
+    from_time= raw_input("Time from hh:mm:ss ")
+    to_date = raw_input("Month to yyyy-mm-dd: ")
+    to_time= raw_input("Time to hh:mm:ss ")
+    return from_date,to_date,from_time,to_time
+
+def meter_statistics(meter_id,api_endpoint,token):
+    meter_stat = [None]
+    headers = {
+               #'Accept': 'application/json',
+               'Content-Type': 'application/json;',
+               'X-Auth-Token': token
+               
+    }
+    from_date,to_date,from_time,to_time=period()   
+    path = "/v2/meters/"+meter_id+"/statistics"
+    target = urlparse(api_endpoint+path)
+    method = 'GET'
+    body = '{"q":[{"field": "timestamp","op": "ge","value": "'+from_date+'T'+from_time+'"},{"field": "timestamp","op": "lt","value": "'+to_date+'T'+to_time+'"}]}'
+    
+    #body='{"q": [{"field": "timestamp","op": "ge","value": "2013-12-28T00:00:00"},{"field": "timestamp","op": "lt","value": "2014-01-05T00:00:00"}]}'
+    h = http.Http()
+    response, content = h.request(target.geturl(),method,body,headers)
+    header = json.dumps(response)
+    json_header = json.loads(header)
+    
+    server_response = json_header["status"]
+    if server_response not in {'200'}:
+        print "Inside meter_statistics(): Something went wrong!"
+        return False, meter_stat
+    else:
+        data = json.loads(content)
+        meter_stat = [None]*len(data)
+        for i in range(len(data)):
+            meter_stat[i]={}
+            meter_stat[i]["average"] = data[i]["avg"]
+            meter_stat[i]["count"] = data[i]["count"]
+            meter_stat[i]["duration"] = data[i]["duration"]
+            meter_stat[i]["duration-end"] = data[i]["duration_end"]
+            meter_stat[i]["duration-start"] = data[i]["duration_start"]
+            meter_stat[i]["max"] = data[i]["max"]
+            meter_stat[i]["min"] = data[i]["min"]
+            meter_stat[i]["period"] = data[i]["period"]
+            meter_stat[i]["period-end"] = data[i]["period_end"]
+            meter_stat[i]["period-start"] = data[i]["period_start"]
+            meter_stat[i]["sum"] = data[i]["sum"]
+            meter_stat[i]["unit"] = data[i]["unit"]
+            meter_stat[i]["group-by"] = data[i]["groupby"]
+        return True, meter_stat
