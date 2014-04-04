34025225ec1debc9e44346e116cc34e6ea1b16f3
teaklvs <tea.kolevska@gmail.com>
2014-04-04 10:17:10 +0200

    django admin

--- web_ui/settings.py
+++ web_ui/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_menu',
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
--- web_ui/urls.py
+++ web_ui/urls.py
@@ -18,6 +18,7 @@ urlpatterns = patterns('',
     (r'^user_page/$', 'main_menu.views.user_page'),
     (r'^auth_token/$', 'main_menu.views.auth_token'),
     (r'^token_data/$', 'main_menu.views.token_data'),
+#    (r'^define_pricing/(?P<ct>\d+)/$', 'main_menu.views.define_pricing', "define_pricing"),
     #(r'^/define_pricing/?ct=(?P<ct>\d+)/$', 'main_menu.views.define_pricing'),
-    (r'^define_pricing/(?P<ct>\d+)/$', 'main_menu.views.define_pricing'),
+    
 )
