f1020f1b9ee12cd8ce309395acb94722a98f4891
teaklvs <tea.kolevska@gmail.com>
2014-07-08 15:09:19 +0200

    mysql db

--- web_ui/settings.py
+++ web_ui/settings.py
@@ -77,9 +77,21 @@ LOGIN_REDIRECT_URL='/index/'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   # 'default': {
+   #     'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}, 
+             
+    'default':{
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'db_cyclops',
+        'USER': 'icclab',
+        'PASSWORD': 'icclab',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+        'OPTIONS': {
+                    "init_command": "SET foreign_key_checks = 0;",
+                    },
     }
 }
 
