8d03c3eb7ffa2e49beb51c9b9fd4b4b35ec680eb
teaklvs <tea.kolevska@gmail.com>
2014-07-29 10:10:50 +0200

    setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #! /usr/local/bin/python
 
+sudo apt-get update
 
+sudo apt-get install -y python-pip
 
 sudo apt-get install python-django
 
@@ -12,10 +14,29 @@ pip install sympy
 
 pip install httplib2
 
+pip install South
+
 sudo apt-get install python2.7-mysqldb
 
 sudo apt-get install mysql-server
 
 mysqladmin create db_cyclops
+
+EXPECTED_ARGS=3
+E_BADARGS=65
+MYSQL=`which mysql`
+ 
+Q1="CREATE DATABASE IF NOT EXISTS $1;"
+Q2="GRANT ALL ON *.* TO '$2'@'localhost' IDENTIFIED BY '$3';"
+Q3="FLUSH PRIVILEGES;"
+SQL="${Q1}${Q2}${Q3}"
+ 
+if [ $# -ne $EXPECTED_ARGS ]
+then
+  echo "Usage: $0 dbname dbuser dbpass"
+  exit $E_BADARGS
+fi
+ 
+$MYSQL -uroot -p -e "$SQL"
 
 python manage.py syncdb --migrate
